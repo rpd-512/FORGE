21bedf5c9a37d297dd64409b10060eee89a26a6c
rpd-512 <rpd-512@users.noreply.github.com>
2025-09-01 23:41:18 +0530

    Plan layout for experiments

---
+++ experiments/experiment_src.h
@@ -0,0 +1,25 @@
+#ifndef EXPERIMENT_SRC_H
+#define EXPERIMENT_SRC_H
+
+// ### Experiment plan ###
+
+// Accuracy
+//      Pre-ADAM
+//      Post-ADAM
+
+// PDF distribution
+//      Before KD–MH
+//      After KD–MH
+
+// Algorithm count
+
+// Probability of KD ADAM (finding a solution under certain threshold)
+
+// Average Time for MH–ADAM vs KD–ADAM (no MH)
+// Count/sec with and without KD optimizations
+
+// How often KD rebuild
+//      Grantt chart of rebuilding, compare fixed time rebuild vs balance based rebuild
+//      Time taken in rebuild KD vs no rebuild KD
+
+#endif // EXPERIMENT_SRC_H
